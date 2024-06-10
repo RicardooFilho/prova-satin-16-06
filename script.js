34c0122a41df64fff4945bf9de26c5a7d4a1621e
RicardooFilho <ricardo.tagami17@gmail.com>
2024-06-09 23:54:19 -0300

    Kick off

---
+++ script.js
@@ -0,0 +1,13 @@
+function openFilterDialog() {
+
+    const dialog = document.getElementById('filter-dialog');
+
+    dialog.showModal();
+}
+
+function closeFilterDialog() {
+
+    const dialog = document.getElementById('filter-dialog');
+
+    dialog.close();
+}
